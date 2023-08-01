bae4787593239ffb61e0ad25d705f4cdedbb2f9c
Fajri Rahman <fajri.achmad55@gmail.com>
2023-08-02 00:33:18 +0700

    initial commit

---
+++ src/main/java/com/nice/commerce/NiceApplication.java
@@ -0,0 +1,17 @@
+package com.nice.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ahmad Fajri Rahman
+ */
+
+@SpringBootApplication
+public class NiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiceApplication.class, args);
+	}
+
+}
